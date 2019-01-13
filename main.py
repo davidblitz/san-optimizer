ced044346d7aab233dd8406fe994ffdbcbe49f78
David Mickisch <dmickisch@gmail.com>
2019-01-13 21:31:02 +0100

    First version for general solution

--- main.py
+++ main.py
@@ -1,9 +1,78 @@
 import numpy as np
 import theano
 import theano.tensor as T
+import itertools
+
+class Node(object):
+    def __init__(self, root=False, name=""):
+        self.root = root
+        self.name = name
+        if root:
+            self.var = theano.shared(np.array([.0]*3), name=name)
+        else:
+            self.var = theano.shared(np.random.randn(3), name=name)
+
+        self.neighbours = []
+        self.visited = False
+
+def createCost(root):
+    to_visit = set([root])
+    visited = set()
+    cost = 0.0
+    tups = []
+    trips = []
+
+    while to_visit:
+        current = to_visit.pop()
+        for n in current.neighbours:
+            if not n in visited:
+                to_visit.add(n)
+                cost += (1 - T.sum((current.var - n.var)**2))**2
+
+        for n, nn in itertools.combinations(current.neighbours, 2):
+            cost += (cos_ta - (n.var - current.var).dot(nn.var - current.var))**2
+
+        visited.add(current)
+
+    updates = []
+    vars = [n.var for n in visited if not n.root]
+    for v in vars:
+        gv = T.grad(cost, v)
+        updates.append((v, v - lr*gv))
+
+    return cost, updates
+
+def nodes_from_mat(mat):
+    nodes = [Node(root=True)] + [Node() for i in range(len(mat)-1)]
+
+    for i, row in enumerate(mat):
+        for j, e in enumerate(row):
+            if e:
+                nodes[i].neighbours.append(nodes[j])
+
+    return nodes
 
 cos_ta = -1.0 / 3.0
 lr = 0.003
+
+#adj_mat = [[0, 1, 0],
+#           [1, 0, 1],
+#           [0, 1, 0]]
+#adj_mat = [[0, 1, 0, 0, 0, 1],
+#           [1, 0, 2, 0, 0, 0],
+#           [0, 1, 0, 1, 0, 0],
+#           [0, 0, 1, 0, 1, 0],
+#           [0, 0, 0, 1, 0, 1],
+#           [1, 0, 0, 0, 1, 0]]
+adj_mat = np.loadtxt("dodecahedron-adjacency2.csv")
+print(adj_mat)
+nodes = nodes_from_mat(adj_mat)
+print(len(nodes))
+for node in nodes:
+    print(node.name, [n.name for n in node.neighbours])
+cost, updates = createCost(nodes[0])
+print(cost)
+"""
 root = theano.shared(np.array([.0, .0, .0]))
 nodes = [theano.shared(np.random.randn(3)) for i in range(2)]
 
@@ -16,22 +85,20 @@ for node in nodes:
   gnode = T.grad(cost, node)
   gnodes.append(gnode)
   updates.append((node, node - lr*gnode))
-  
+"""
 minimize = theano.function(
         inputs=[],
-
         outputs=[cost],
-        updates= updates )
-#p1_init = np.randn(3)
-#p2_init = np.randn(3)
+        updates= updates)
+print("Minimizing Objective...")
 obj = minimize()[0]
 while obj > 1e-5:
     obj = minimize()[0]
     print(obj)
 
 print("Final Positions: ")
-root_v = root.get_value()
-nodes_v = [node.get_value() for node in nodes]
+#root_v = nodes[0].var.get_value()
+nodes_v = {chr(ord("A") + i) : node.var.get_value().tolist() for i, node in enumerate(nodes)}
 print("nodes: ", nodes_v)
-print("dist(node1, node2) = ", np.sum((nodes_v[0] - nodes_v[1])**2))
-print("cos(root - node1, node1 - node2) = ", (nodes_v[0] -nodes_v[1]).dot(root_v - nodes_v[0]))
+#print("dist(node0, node1) = ", np.sum((nodes_v[0] - nodes_v[1])**2))
+#print("cos(root - node1, node1 - node2) = ", (nodes_v[1] -nodes_v[2]).dot(nodes_v[0] - nodes_v[1]))
